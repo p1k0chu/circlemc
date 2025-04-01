06f04997825233aafbc3b80aee34854e94ed9727
p1k0chu <anatoliyid0207@gmail.com>
2025-04-01 20:22:15 +0300

    first commit

---
+++ src/main/java/com/github/p1k0chu/circlemc/CircleMC.java
@@ -0,0 +1,16 @@
+package com.github.p1k0chu.circlemc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CircleMC implements ModInitializer {
+	public static final String MOD_ID = "circlemc";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
